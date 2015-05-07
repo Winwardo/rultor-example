0fcb9184be3646fe5fbc291df7979f127ba64a8f
Topher Winward <winwardo@gmail.com>
2015-05-07 19:39:01 +0100

    Calculator base

---
+++ src/main/java/rultor/example/Calculator.java
@@ -0,0 +1,15 @@
+package rultor.example;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add(int a, int b) {
+	return a + b;
+    }
+
+    public int subtract(int a, int b) {
+	return a - b;
+    }
+}
---
+++ src/test/java/rultor/example/CalculatorTests.java
@@ -0,0 +1,20 @@
+package rultor.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTests {
+
+    @Test
+    public void CalculatorAddSimple() {
+	Calculator calculator = new Calculator();
+	assertEquals(12, calculator.add(5, 7));
+    }
+
+    @Test
+    public void CalculatorAddLargeNumbers() {
+	Calculator calculator = new Calculator();
+	assertEquals(12000, calculator.add(5000, 7000));
+    }
+}
