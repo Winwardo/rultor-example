8533aa56f501eff7d997a25f77f994595322377a
Topher Winward <winwardo@gmail.com>
2015-05-07 19:56:52 +0100

    Adding jUnit

--- src/test/java/rultor/example/CalculatorTest.java
+++ src/test/java/rultor/example/CalculatorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class CalculatorTests {
+public class CalculatorTest {
 
     @Test
     public void CalculatorAddSimple() {
